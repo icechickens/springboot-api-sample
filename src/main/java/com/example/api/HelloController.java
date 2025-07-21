8f7f0f79b2b1869e9f91a5d6e34b2e67dcf86cd9
Chikiri Hideya <c.hideya.ice@gmail.com>
2025-07-21 17:22:24 +0900

    SpringからDBに接続

--- src/main/java/com/example/api/HelloController.java
+++ src/main/java/com/example/api/HelloController.java
@@ -1,20 +1,28 @@
-
 package com.example.api;
 
+import com.example.api.entities.GreetingEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * http://localhost:8080/api/hello?lang=ja でアクセス
+ * 以下でローカルブラウザから接続可能
+ * http://localhost:8080/api/hello?lang=en
+ * http://localhost:8080/api/hello?lang=ja
  */
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+
+    @Autowired
+    private GreetingRepository repository;
+
     @RequestMapping("/hello")
     public String hello(
             @RequestParam String lang
     ) {
-        return "HELLO";
+        GreetingEntity entity = this.repository.findFirstByLang(lang);
+        return entity.text;
     }
 }
